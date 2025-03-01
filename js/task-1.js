7b2125688adca0c3f0c65812fc49fcec80c68d8b
Devid Hainal <devidhainal8@gmail.com>
2025-03-01 16:29:47 +0200

    correct tast-1 JS & CSS

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,21 @@
-const  categories = document.querySelectorAll("#categories .item")
-console.log("Number of categories:",categories.length);
+const categories = document.querySelectorAll("#categories .item");
+console.log("Number of categories:", categories.length);
 
 categories.forEach(category => {
-    const title = category.querySelector("h2")
+    const title = category.querySelector("h2");
+    title.classList.add("title-category");
     console.log("Category:", title.textContent);
-    const items = category.querySelectorAll("li")
-    console.log("Elements:", items.length);
     
+    const items = category.querySelectorAll("li");
+    items.forEach(item => {
+        item.classList.add('items-category');
+    });
+    console.log("Elements:", items.length);
+
+    const listItem = category.querySelector('ul');
+    if (listItem) {
+        listItem.classList.add('categories-sublist');
+    }
 });
 
 
